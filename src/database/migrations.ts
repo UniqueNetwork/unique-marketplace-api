40798af47091067dc2f285ae7afb71d92faa293d
icehuntmen <icehuntmen@gmail.com>
2022-01-25 13:08:11 +0300

    Added managed postgres migration for deployment

--- src/database/migrations.ts
+++ src/database/migrations.ts
@@ -1,13 +1,17 @@
 import { createConnection } from 'typeorm';
 
-import { ProjectNamingStrategy} from './naming_strategy';
+import { ProjectNamingStrategy } from './naming_strategy';
 import { activeMigrations } from '../migrations';
 
 export const runMigrations = async (config) => {
-  const connection = await createConnection({
-    name: 'migrations', type: 'postgres', url: config.postgresUrl, logging: true, migrations: activeMigrations,
-    namingStrategy: new ProjectNamingStrategy()
-  });
-  await connection.runMigrations();
-  await connection.close();
-}
+    const connection = await createConnection({
+        name: 'migrations',
+        type: 'postgres',
+        url: config.postgresUrl,
+        logging: true,
+        migrations: activeMigrations,
+        namingStrategy: new ProjectNamingStrategy(),
+    });
+    await connection.runMigrations();
+    await connection.close();
+};
--- src/main.ts
+++ src/main.ts
@@ -7,23 +7,28 @@ import { runMigrations } from './database/migrations';
 import { ignoreQueryCase, useGlobalPipes } from './utils/application';
 
 const initSwagger = (app: INestApplication, config) => {
-  const swaggerConf = new DocumentBuilder().setTitle(config.swagger.title).setDescription(config.swagger.description).setVersion(config.swagger.version).build();
-  const swaggerDocument = SwaggerModule.createDocument(app, swaggerConf);
-  SwaggerModule.setup('api/docs/', app, swaggerDocument);
-}
+    const swaggerConf = new DocumentBuilder()
+        .setTitle(config.swagger.title)
+        .setDescription(config.swagger.description)
+        .setVersion(config.swagger.version)
+        .build();
+    const swaggerDocument = SwaggerModule.createDocument(app, swaggerConf);
+    SwaggerModule.setup('api/docs/', app, swaggerDocument);
+};
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule), config = app.get('CONFIG');
+    const app = await NestFactory.create(AppModule),
+        config = app.get('CONFIG');
 
-  if(config.autoDBMigrations) await runMigrations(config);
+    //if(config.autoDBMigrations) await runMigrations(config);
 
-  if(config.disableSecurity) app.enableCors();
+    if (config.disableSecurity) app.enableCors();
 
-  initSwagger(app, config);
-  ignoreQueryCase(app);
-  useGlobalPipes(app);
+    initSwagger(app, config);
+    ignoreQueryCase(app);
+    useGlobalPipes(app);
 
-  await app.listen(config.listenPort);
+    await app.listen(config.listenPort);
 }
 
 bootstrap();
