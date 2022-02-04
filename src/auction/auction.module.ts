7a7b4a5898cbf47dc7e97680ad442c23177e16fd
Arseny Medoev <a.medoev@corp.mail.ru>
2022-02-04 12:05:57 +0300

    add uniqueApiProvider

--- src/auction/auction.module.ts
+++ src/auction/auction.module.ts
@@ -4,12 +4,20 @@ import { BidPlacingService } from './services/bid-placing.service';
 import { AuctionController } from './auction.controller';
 import { ScheduleModule } from '@nestjs/schedule';
 import { AuctionClosedService } from './services/auction-closed.service';
+import { uniqueApiProvider } from './providers/unique-api-provider';
+import { ConfigModule } from "../config/module";
 
 @Module({
   imports: [
+    ConfigModule,
     ScheduleModule.forRoot()
   ],
-  providers: [AuctionCreationService, BidPlacingService, AuctionClosedService],
+  providers: [
+    AuctionCreationService,
+    BidPlacingService,
+    AuctionClosedService,
+    uniqueApiProvider,
+  ],
   controllers: [AuctionController]
 })
 export class AuctionModule {}
