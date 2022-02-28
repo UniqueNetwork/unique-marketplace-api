b0348b94bf14153dd5b39ae198ec698bfbe86810
Arseny Medoev <a.medoev@corp.mail.ru>
2022-02-28 14:01:00 +0300

    remove unused module - auction-closed

--- src/auction-close/interface/bid.interface.ts
+++
@@ -1,8 +0,0 @@
-import { Bid } from "../../auction/types";
-
-export interface BidInterface {
-  bids: Array<Partial<Bid>>,
-  winer(): Partial<Bid>,
-  minting():Partial<Bid>,
-  lose(): Array<Partial<Bid>>
-}
