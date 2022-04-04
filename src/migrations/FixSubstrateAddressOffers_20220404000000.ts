af3746bf56ff994c4207ac53d0de10d68abd1107
Alexander Shumilin <icehuntmen@gmail.com>
2022-04-04 16:59:49 +0300

    Fix name migration and encoderAddress

--- src/migrations/FixSubstrateAddressOffers_20220404000000.ts
+++ src/migrations/FixSubstrateAddressOffers_20220404000000.ts
@@ -2,8 +2,8 @@ import { MigrationInterface, QueryRunner, Table } from 'typeorm';
 import { encodeAddress, decodeAddress } from '@polkadot/util-crypto';
 import { ContractAsk } from '../entity';
 
-export class FixSubstrateAddressOffers_20220325000000 implements MigrationInterface {
-  name = 'FixSubstrateAddressOffers_20220325000000';
+export class FixSubstrateAddressOffers_20220404000000 implements MigrationInterface {
+  name = 'FixSubstrateAddressOffers_20220404000000';
 
   async up(queryRunner: QueryRunner): Promise<void> {
     for (let offers of await queryRunner.query(`SELECT * FROM public."contract_ask"`)) {
