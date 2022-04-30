381f6aecacabef7a5ac4e20776c96460647cd88d
Alexander Shumilin <icehuntmen@gmail.com>
2022-04-30 10:50:34 +0300

    Remove import modules

--- src/cli.module.ts
+++ src/cli.module.ts
@@ -1,26 +1,14 @@
-import { Module, MiddlewareConsumer, NestModule } from '@nestjs/common';
-import { CommandModule } from 'nestjs-command';
-import { TerminusModule } from '@nestjs/terminus';
-import { HttpModule } from '@nestjs/axios';
-import { ServeStaticModule } from '@nestjs/serve-static';
-import { join } from 'path';
+import {Module} from '@nestjs/common';
+import {CommandModule} from 'nestjs-command';
+import {ServeStaticModule} from '@nestjs/serve-static';
+import {join} from 'path';
 
-import { DatabaseModule } from './database/module';
-import { ConfigModule } from './config/module';
-import { PlaygroundCommand } from './utils/playground';
-import { SentryLoggerService } from './utils/sentry/sentry-logger.service';
-import { PrometheusService } from './utils/prometheus/prometheus.service';
+import {DatabaseModule} from './database/module';
+import {ConfigModule} from './config/module';
+import {PlaygroundCommand} from './utils/playground';
+import {SentryLoggerService} from './utils/sentry/sentry-logger.service';
 
-import { EscrowModule } from './escrow/module';
-import { SettingsController, SettingsService } from './settings';
-import { OffersController, OffersService } from './offers';
-import { TradesController, TradesService } from './trades';
-import { HealthController, HealthService } from './utils/health';
-import { MetricsController, MetricsService } from './utils/metrics';
-import { AuctionModule } from "./auction/auction.module";
-import { BroadcastModule } from "./broadcast/broadcast.module";
-import { RequestLoggerMiddleware } from "./utils/logging/request-logger-middleware.service";
-import {ApiPromise} from "@polkadot/api";
+import {EscrowModule} from './escrow/module';
 
 @Module({
   imports: [
