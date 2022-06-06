b5614ca78b59b02d42c99219a8ce65f778a909c2
Alex Sharypin <alexsharypin>
2022-06-06 10:32:59 +0200

    feat: mass fixprice sale

---
+++ src/admin/constants.ts
@@ -0,0 +1,1 @@
+export const U32_MAX_VALUE = 4294967295;
--- src/admin/pipes/parse-collection-id.pipe.ts
+++ src/admin/pipes/parse-collection-id.pipe.ts
@@ -1,12 +1,13 @@
 import { PipeTransform, Injectable, ArgumentMetadata, BadRequestException } from '@nestjs/common';
 import { isNumber, isInt, isPositive } from 'class-validator';
+import { U32_MAX_VALUE } from '../constants';
 
 @Injectable()
 export class ParseCollectionIdPipe implements PipeTransform<string, number> {
   transform(param: string, _metadata: ArgumentMetadata): number {
     const value = Number(param);
 
-    if (!isNumber(value) || !isInt(value) || !isPositive(value) || value > 4294967295) throw new BadRequestException('Please enter valid ID');
+    if (!isNumber(value) || !isInt(value) || !isPositive(value) || value > U32_MAX_VALUE) throw new BadRequestException('Please enter valid ID');
 
     return value;
   }
