817054644eb5af13ec3f3631d288bb5893d6e546
Alexander Shumilin <icehuntmen@gmail.com>
2022-06-19 14:59:32 +0300

    fix RPC types

--- src/utils/blockchain/rpc.ts
+++ src/utils/blockchain/rpc.ts
@@ -1,8 +1,7 @@
 import * as mainnetTypes from '@unique-nft/unique-mainnet-types/definitions';
 import * as opalTypes from '@unique-nft/opal-testnet-types/definitions';
-import * as quartzTypes from '@unique-nft/quartz-mainnet-types/definitions';
 
-export const getRpc = function (type: string) {
+export function RPC(type: string) {
   if (type === 'quartz') {
     return mainnetTypes.unique.rpc; //quartzTypes.unique.rpc;
   }
@@ -10,4 +9,4 @@ export const getRpc = function (type: string) {
     return opalTypes.unique.rpc;
   }
   return mainnetTypes.unique.rpc;
-};
+}
