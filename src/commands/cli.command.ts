65328b18b148b32314e6ee6db03ec2e5bab221a2
Alexander Shumilin <icehuntmen@gmail.com>
2022-06-27 19:25:03 +0300

    Change types in commands

--- src/commands/cli.command.ts
+++ src/commands/cli.command.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@nestjs/common';
 import { CheckConfigCommandService } from './services/check-config.service';
-import { Command, Option, Positional } from 'nestjs-command';
+import { Command, CommandPositionalOption, Option, Positional } from 'nestjs-command';
 import { CollectionCommandService } from './services/collection.service';
 import { DeployContractService } from './services/deploy-contract.service';
 
@@ -12,6 +12,9 @@ export class CliCommands {
     private readonly deployContractService: DeployContractService,
   ) {}
 
+  /**
+   *
+   */
   @Command({
     command: 'check config',
     describe: 'Checking the collection configuration',
@@ -20,46 +23,9 @@ export class CliCommands {
     await this.checkoutConfigService.checkoutCollecetionMain();
   }
 
-  @Command({
-    command: 'checkout colllection <id>',
-    describe: 'Get collection data',
-  })
-  async showCollection(
-    @Positional({
-      name: 'id',
-      describe: 'ID collection',
-      type: 'string',
-    })
-    collection: string,
-    @Option({
-      name: 'token',
-      describe: 'Get token ID',
-      type: 'string',
-      alias: 't',
-      required: false,
-    })
-    token: string,
-    @Option({
-      name: 'depth',
-      describe: 'Object view depth',
-      type: 'number',
-      alias: 'd',
-      default: 1,
-      required: false,
-    })
-    depth: number,
-    @Option({
-      name: 'wsEndpoint',
-      describe: 'WSS Endpoint',
-      type: 'string',
-      alias: 'w,wss',
-      required: false,
-    })
-    wss: string,
-  ) {
-    await this.collectionService.showCollection({ collection, token, depth, wss });
-  }
-
+  /**
+   * Deploy contract
+   */
   @Command({
     command: 'deploy contract',
     describe: 'Deploy contract',
@@ -67,5 +33,37 @@ export class CliCommands {
   async deployContract() {
     await this.deployContractService.init();
     await this.deployContractService.deploy();
+  }
+
+  /**
+   *
+   * @param collection
+   * @param token
+   * @param depth
+   * @param wss
+   */
+  @Command({
+    command: 'check collection <id>',
+    describe: 'Get collection data',
+  })
+  async showCollections(
+    @Positional({ name: 'id', describe: 'ID collection', type: 'string' } as CommandPositionalOption)
+    collection: string,
+    @Option({ name: 'token', describe: 'Get token ID', type: 'string', alias: 't', required: false } as CommandPositionalOption)
+    token: string,
+    @Option({
+      name: 'depth',
+      describe: 'Object view depth',
+      type: 'string',
+      alias: 'd',
+      default: 1,
+      required: false,
+    } as CommandPositionalOption)
+    depth: number,
+    @Option({ name: 'wsEndpoint', describe: 'WSS Endpoint', type: 'string', alias: 'w', required: false } as CommandPositionalOption)
+    wss: string,
+  ) {
+    const data = { collection, token, depth, wss };
+    await this.collectionService.showCollection(data);
   }
 }
