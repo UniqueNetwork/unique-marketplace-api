065cd3cff36aa8d970fe06aa4c6cdee3828d3313
Alexander Shumilin <icehuntmen@gmail.com>
2022-07-19 12:28:30 +0300

    Convert add

--- src/migrations/ConvertOffersAndAuction_20220719300000.ts
+++ src/migrations/ConvertOffersAndAuction_20220719300000.ts
@@ -4,6 +4,7 @@ export class ConvertOffersAndAuction_20220719300000 implements MigrationInterfac
   name = 'ConvertOffersAndAuction_22071930000000';
 
   async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.startTransaction();
     try {
       await queryRunner.query(
         "INSERT INTO offers ( SELECT ask.ID AS id ,ask.status AS status,CASE WHEN auc.ID NOTNULL THEN 'Auction' ELSE 'FixedPrice' END AS TYPE,ask.collection_id AS collection_id,ask.token_id AS token_id,ask.network AS network,ask.price AS price,ask.currency AS currency,auc.price_step,auc.start_price,auc.status AS ACTION,auc.stop_at AS stop_at,auc.updated_at AS updated_auction,ask.address_from AS address_from,ask.address_to AS address_to,CASE WHEN ask.ID NOTNULL THEN NULL END AS address_contract,CASE WHEN ask.block_number_ask !='-1' THEN ask.block_number_ask END AS block_number_ask,ask.block_number_cancel,ask.block_number_buy,ask.created_at_ask,ask.updated_at,CASE WHEN auc.ID NOTNULL THEN json_build_object('auction_id',auc.ID) ELSE '{}' END AS collection_data FROM contract_ask ask LEFT JOIN auctions auc ON ask.ID=auc.contract_ask_id)",
@@ -17,7 +18,5 @@ export class ConvertOffersAndAuction_20220719300000 implements MigrationInterfac
     }
   }
 
-  async down(queryRunner: QueryRunner): Promise<void> {
-
-  }
+  async down(queryRunner: QueryRunner): Promise<void> {}
 }
