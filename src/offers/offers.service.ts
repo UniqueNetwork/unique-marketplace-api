24a03c40840d6b66e5c80665ff3d3fce79f5c583
Anton Drelin <antondrelin@gmail.com>
2022-07-20 20:54:58 +0300

    fix: Offers

--- src/offers/offers.service.ts
+++ src/offers/offers.service.ts
@@ -72,7 +72,7 @@ export class OffersService {
   }
 
   private auctionIds(items: Array<any>): Array<number> {
-    return items.filter((item) => item?.offer_type == 'Auction').map((item) => item?.offer_id);
+    return items.filter((item) => item?.auction_id !== null).map((item) => item?.offer_id);
   }
 
   private async bids(auctionIds: Array<number>): Promise<Array<Partial<Bid>>> {
@@ -201,7 +201,7 @@ export class OffersService {
             }),
         };
 
-        if (item.offer_type === 'Auction') {
+        if (item.auction_id !== null) {
           obj.auction = Object.assign(
             {},
             {
